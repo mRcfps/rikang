3c6bd921355f41a6f9d14370f9c4bd81f2f0a521
mRcfps <1402491442@qq.com>
2017-06-23 11:35:17 +0800

    Init serializers for users app.

---
+++ users/serializers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Doctor, Patient
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        write_only_fields = ('password',)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Doctor
+        fields = ('id', 'name', 'avatar', 'department', 'years',
+                  'title', 'hospital', 'ratings', 'patient_num')
+
+
+class PatientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Patient
+        fields = ('id', 'name', 'avatar')
+
+
+class InformationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Information
+        fields = '__all__'
