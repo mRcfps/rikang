5a835edc6a36a2fce663848ea981ea2740b148cf
mRcfps <1402491442@qq.com>
2017-06-23 22:02:32 +0800

    Remove id in list_display.

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from .models import Doctor, Patient
 
 class DoctorAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'department', 'name', 'hospital',
+    list_display = ('department', 'name', 'hospital',
                     'title', 'ratings', 'patient_num', 'created')
     list_filter = ('department', 'hospital', 'title', 'ratings')
     search_fields = ('name',)
@@ -13,7 +13,7 @@ class DoctorAdmin(admin.ModelAdmin):
 
 class PatientAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'name', 'created')
+    list_display = ('name', 'created')
 
 admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(Patient, PatientAdmin)
