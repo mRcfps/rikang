8bc573dd1df22736dcd2297a43592e3df49f5d67
mRcfps <1402491442@qq.com>
2017-06-24 10:48:32 +0800

    Add namespace to global urls.

--- rikang/urls.py
+++ rikang/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^home/', include('home.urls')),
-    url(r'^users/', include('users.urls')),
+    url(r'^home/', include('home.urls', namespace='home')),
+    url(r'^users/', include('users.urls', namespace='users')),
     url(r'^admin/', admin.site.urls),
 ]
