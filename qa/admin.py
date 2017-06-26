4de3e038b67e4027117b8b6fc8bf83819233f7f5
mRcfps <1402491442@qq.com>
2017-06-26 21:28:53 +0800

    Init admin for qa app.

---
+++ qa/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Question, Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title', 'department', 'patient',
+                    'solved', 'stars', 'updated', 'created')
+    list_filter = ('department', 'solved', 'stars', 'created')
+    search_fields = ('title', 'department')
+
+admin.site.register(Answer, AnswerAdmin)
