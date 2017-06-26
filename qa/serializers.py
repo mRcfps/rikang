b276ea2471d1f2f0a2efe0553e58206252c6fdbd
mRcfps <1402491442@qq.com>
2017-06-26 17:03:48 +0800

    Add serializers for qa app.

---
+++ qa/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Question, Answer
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Question
+        fields = ('id', 'title', 'department', 'body',
+                  'solved', 'stars', 'created')
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = ('id', 'question', 'author', 'author_name',
+                  'author_info', 'diagnosis', 'prescription',
+                  'course', 'advice', 'picked', 'upvotes', 'created')
