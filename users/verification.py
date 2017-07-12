13d4cfa262e8c6ac3db4a73fcba772a93f36f38f
mRcfps <1402491442@qq.com>
2017-07-12 15:37:01 +0800

    Make SMS_API_KEY a separate file  to prevent evil intentions.

--- users/verification.py
+++ users/verification.py
@@ -1,10 +1,11 @@
+import os
 import random
 
-from yunpian import ClientV2
+from users.yunpian import ClientV2
 
 COMPANY_NAME = '日康在线'
 
-API_KEY = 'Please contact mRcfps.'
+API_KEY_PATH = '../SMS_API_KEY'
 
 # SMS Verification Code (1001~9999)
 sms_code = random.randrange(1001, 9999)
@@ -12,10 +13,17 @@ sms_code = random.randrange(1001, 9999)
 
 def send_sms_code(phone):
     """Send sms code to the phone and return response status."""
-    client = ClientV2(API_KEY)
+
+    if not os.path.exists(API_KEY_PATH):
+        raise FileNotFoundError('Please contact mrc to get sms api key.')
+
+    with open(API_KEY_PATH, 'r') as f:
+        api_key = f.read()
+
+    client = ClientV2(api_key)
     sms_code = random.randrange(1001, 9999)
     context = {'company': COMPANY_NAME, 'code': sms_code}
-    response = client.send_tpl_sms(phone, tpl_id=2, context)
+    response = client.send_tpl_sms(phone, tpl_id=2, context=context)
 
     return response.status_code
 
