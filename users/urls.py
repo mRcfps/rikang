b5a52c36a034730b26546b336b5ddd749906aaf2
mRcfps <1402491442@qq.com>
2017-07-12 14:48:01 +0800

    Add comments to each urlpattern and improve consistence.

--- users/urls.py
+++ users/urls.py
@@ -3,68 +3,93 @@ from django.conf.urls import url
 from users import views
 
 urlpatterns = [
+    # `POST` /users/register
     url(
         r'^register$',
         views.UserRegistrationView.as_view(),
         name='register'
     ),
+
+    # `POST` /users/login/
     url(
         r'^login/$',
         views.UserLoginView.as_view(),
         name='login'
     ),
+
+    # `PUT` /users/change-password
     url(
-        r'change-password$',
+        r'^change-password$',
         views.UserChangePasswordView.as_view(),
         name='change-password'
     ),
+
+    # `POST` /users/request-sms-code
     url(
-        r'request-sms-code$',
+        r'^request-sms-code$',
         views.RequestSmsCodeView.as_view(),
         name='request-sms-code'
     ),
+
+    # `POST` /users/verify-sms-code
     url(
-        r'verify-sms-code$',
+        r'^verify-sms-code$',
         views.VerifySmsCodeView.as_view(),
         name='verify-sms-code'
     ),
+
+    # `POST` /users/doctor/init
     url(
         r'^doctor/init$',
         views.DoctorInitView.as_view(),
         name='doctor-init'
     ),
+
+    # `GET``PUT` /users/doctor/profile/
     url(
         r'^doctor/profile/$',
         views.DoctorProfileView.as_view(),
         name='doctor-profile'
     ),
+
+    # `GET``PUT` /users/doctor/info/
     url(
         r'^doctor/info/$',
         views.DoctorInfoView.as_view(),
         name='doctor-info'
     ),
+
+    # `GET``PUT` /users/patient/profile/
     url(
-        r'patient/profile/$',
+        r'^patient/profile/$',
         views.PatientProfileView.as_view(),
         name='patient-profile'
     ),
+
+    # `GET` /users/patient/questions/
     url(
-        r'patient/questions/$',
+        r'^patient/questions/$',
         views.PatientQuestionsView.as_view(),
         name='patient-questions'
     ),
+
+    # `GET` /users/patient/starred-questions/
     url(
-        r'patient/starred-questions/$',
+        r'^patient/starred-questions/$',
         views.PatientStarredQuestionsView.as_view(),
         name='patient-starred-questions'
     ),
+
+    # `GET` /users/patient/fav-doctors/
     url(
-        r'patient/fav-doctors/$',
+        r'^patient/fav-doctors/$',
         views.PatientFavDoctorsView.as_view(),
         name='patient-fav-doctors'
     ),
+
+    # `GET` /users/patient/fav-posts/
     url(
-        r'patient/fav-posts/$',
+        r'^patient/fav-posts/$',
         views.PatientFavPostsView.as_view(),
         name='patient-fav-posts'
     ),
