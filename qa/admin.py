6707d617ba8759385ae01e35d12d2041a0909183
mRcfps <1402491442@qq.com>
2017-07-12 10:32:47 +0800

    Fix a bug in QuestionAdmin.

--- qa/admin.py
+++ qa/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import Question, Answer
+from .models import Question, Answer, AnswerComment
 
 
 class QuestionAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'title', 'department', 'patient',
+    list_display = ('id', 'title', 'department', 'questioner',
                     'solved', 'stars', 'updated', 'created')
     list_filter = ('department', 'solved', 'stars', 'created')
     search_fields = ('title', 'department')
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(AnswerComment)
