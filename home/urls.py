155842e8195b22c6c6e7bfda27d33bffcac96702
mRcfps <1402491442@qq.com>
2017-07-12 14:58:28 +0800

    Improve home urls.

--- home/urls.py
+++ home/urls.py
@@ -3,68 +3,93 @@ from django.conf.urls import url
 from home import views
 
 urlpatterns = [
+    # `GET` /home/posts/
     url(
         r'^posts/$',
         views.PostListView.as_view(),
         name='post-list'
     ),
+
+    # `GET` /home/posts/{post_id}/
     url(
         r'^posts/(?P<pk>\d+)/$',
         views.PostDetailView.as_view(),
         name='post-detail'
     ),
+
+    # `GET` /home/posts/{post_id}/fav
     url(
         r'^posts/(?P<pk>\d+)/fav$',
         views.PostFavView.as_view(),
         name='post-fav'
     ),
+
+    # `GET` /home/hospitals/
     url(
         r'^hospitals/$',
         views.HospitalListView.as_view(),
         name='hospital-list'
     ),
+
+    # `GET` /home/hospitals/{hospital_id}/
     url(
         r'^hospitals/(?P<pk>\d+)/$',
         views.HospitalDetailView.as_view(),
         name='hospital-detail'
     ),
+
+    # `GET` /home/hospitals/{hospital_id}/doctors/
     url(
-        r'hospitals/(?P<pk>\d+)/doctors/$',
+        r'^hospitals/(?P<pk>\d+)/doctors/$',
         views.HospitalDoctorsView.as_view(),
         name='hospital-doctors'
     ),
+
+    # `GET` /home/doctors/
     url(
-        r'doctors/$',
+        r'^doctors/$',
         views.DoctorListView.as_view(),
         name='doctor-list'
     ),
+
+    # `GET` /home/doctors/{doctor_id}/
     url(
-        r'doctors/(?P<pk>\d+)/$',
+        r'^doctors/(?P<pk>\d+)/$',
         views.DoctorDetailView.as_view(),
         name='doctor-detail'
     ),
+
+    # `GET` /home/doctors/{doctor_id}/info/
     url(
-        r'doctors/(?P<pk>\d+)/info/$',
+        r'^doctors/(?P<pk>\d+)/info/$',
         views.DoctorInfoView.as_view(),
         name='doctor-info'
     ),
+
+    # `GET` /home/doctors/{doctor_id}/fav
     url(
-        r'doctors/(?P<pk>\d+)/fav$',
+        r'^doctors/(?P<pk>\d+)/fav$',
         views.DoctorFavView.as_view(),
         name='doctor-fav'
     ),
+
+    # `GET` /home/doctors/{doctor_id}/answers/
     url(
-        r'doctors/(?P<pk>\d+)/answers/$',
+        r'^doctors/(?P<pk>\d+)/answers/$',
         views.DoctorAnswersView.as_view(),
         name='doctor-answers'
     ),
+
+    # `GET` /home/doctors/{doctor_id}/comments/
     url(
-        r'doctors/(?P<pk>\d+)/comments/$',
+        r'^doctors/(?P<pk>\d+)/comments/$',
         views.DoctorCommentsView.as_view(),
         name='doctor-comments'
     ),
+
+    # `POST` /home/doctors/{doctor_id}/comments/new
     url(
-        r'doctors/(?P<pk>\d+)/comments/new$',
+        r'^doctors/(?P<pk>\d+)/comments/new$',
         views.DoctorNewCommentView.as_view(),
         name='doctor-new-comment'
     ),
