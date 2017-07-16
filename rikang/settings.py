8db35ebe37314c16752d7f68c89b0cefd4057fdb
mRcfps <1402491442@qq.com>
2017-07-16 16:32:28 +0800

    Switch back to sqlite3 for development.

--- rikang/settings.py
+++ rikang/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'rikang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rikang',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
