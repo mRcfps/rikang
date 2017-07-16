448f882e04cfcaee9e8f09cf4bb52b768c5f4dcc
mRcfps <1402491442@qq.com>
2017-07-16 09:24:22 +0800

    Switch db backend to PostgreSQL.

--- rikang/settings.py
+++ rikang/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     # 3rd-party apps
     'rest_framework',
@@ -85,8 +86,12 @@ WSGI_APPLICATION = 'rikang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rikang',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
