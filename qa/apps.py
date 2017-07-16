8a6e9c0d588bb41d74baf967e30f9e9ca428210a
mRcfps <1402491442@qq.com>
2017-07-16 21:20:34 +0800

    Add verbose_name for each app.

--- qa/apps.py
+++ qa/apps.py
@@ -5,6 +5,7 @@ from elasticsearch_dsl.connections import connections
 
 class QaConfig(AppConfig):
     name = 'qa'
+    verbose_name = "问答社区"
 
     def ready(self):
         connections.create_connection()
