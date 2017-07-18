4885942eace362bb25d12da23f406e362bfd6f1a
mRcfps <1402491442@qq.com>
2017-07-18 15:17:09 +0800

    Base implementation of services admin.

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from services.models import Order, Consultation, Summary
+
+
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = ('order_no', 'service_type', 'owner',
+                    'cost', 'status', 'created')
+    list_filter = ('status', 'service_type')
+
+
+class ConsultationAdmin(admin.ModelAdmin):
+
+    list_display = ('doctor', 'patient', 'created')
+
+
+class SummaryAdmin(admin.ModelAdmin):
+
+    list_display = ('summary_type', 'charges_amount', 'charges_count', 'created')
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Consultation, ConsultationAdmin)
+admin.site.register(Summary, SummaryAdmin)
