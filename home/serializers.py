c06a345c7e69bab321806ad9c614b3034622d53e
mRcfps <1402491442@qq.com>
2017-07-21 19:49:27 +0800

    Calculate new ratings for the doctor immediately after a DoctorComment is saved.

--- home/serializers.py
+++ home/serializers.py
@@ -99,4 +99,4 @@ class NewCommentDisplaySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = DoctorComment
-        fields = ('anonymous', 'order_no', 'ratings', 'body')
+        fields = ('anonymous', 'ratings', 'body')
--- home/views.py
+++ home/views.py
@@ -168,6 +168,13 @@ class DoctorNewCommentView(generics.CreateAPIView):
     def perform_create(self, serializer):
         doctor = get_object_or_404(Doctor, id=self.kwargs['pk'], active=True)
         serializer.save(patient=self.request.user.patient, doctor=doctor)
+
+        # calculate new rating for this doctor
+        new_rating = serializer.validated_data['ratings']
+        comment_num = doctor.comments.count()
+        doctor.ratings = (doctor.ratings * (comment_num - 1) + new_rating) / comment_num
+        doctor.save()
+
         push.send_push_to_user(
             message="有位用户刚刚对您做出了评价。",
             user_id=doctor.user.id
