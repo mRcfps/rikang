22f553f71e790e1abb52036c43f1a60743998fce
mRcfps <1402491442@qq.com>
2017-07-22 11:35:34 +0800

    Ignore static files.

--- rikang/settings.py
+++ rikang/settings.py
@@ -130,6 +130,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Media settings
