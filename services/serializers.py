f799c1d4684ad1d53fa6a1bed8871dd6ad27b36d
mRcfps <1402491442@qq.com>
2017-08-02 12:13:07 +0800

    Reshape serializers about orders and consultation.

--- services/serializers.py
+++ services/serializers.py
@@ -1,21 +1,17 @@
 from rest_framework import serializers
 
 from services.models import Order, Consultation, Comment
+from home.serializers import CommentDisplaySerializer
 from users.serializers import PatientSerializer
 
 
 class ConsultationSerializer(serializers.ModelSerializer):
 
     patient = PatientSerializer()
-    status = serializers.SerializerMethodField()
 
     class Meta:
         model = Consultation
         fields = ('doctor', 'patient', 'status')
-
-    def get_status(self, consult):
-        order = Order.objects.get(order_no=consult.id)
-        return order.status
 
 
 class ConsultationOrderSerializer(serializers.ModelSerializer):
@@ -23,11 +19,12 @@ class ConsultationOrderSerializer(serializers.ModelSerializer):
     order_no = serializers.SerializerMethodField()
     service_type = serializers.CharField(source='service_type.model')
     service_object = ConsultationSerializer()
+    comment = CommentDisplaySerializer()
 
     class Meta:
         model = Order
         fields = ('order_no', 'owner', 'cost', 'status', 'created',
-                  'service_type', 'service_object')
+                  'service_type', 'service_object', 'comment')
 
     def get_order_no(self, order):
         return order.order_no.hex
