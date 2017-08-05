912a99363b002c8b24639a1b45fec8cdd866f9ee
mRcfps <1402491442@qq.com>
2017-08-05 10:42:17 +0800

    Bug fix in patient-services api.

--- services/serializers.py
+++ services/serializers.py
@@ -11,7 +11,7 @@ class ConsultationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Consultation
-        fields = ('doctor', 'patient', 'status')
+        fields = ('doctor', 'patient')
 
 
 class ConsultationOrderSerializer(serializers.ModelSerializer):
